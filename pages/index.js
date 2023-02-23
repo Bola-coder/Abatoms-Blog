ad3c65debeecf317459262d89725412dbc9cb7ff
Ahmed Bolarinwa <bolarinwaahmed22@gmail.com>
2023-02-23 16:13:30 +0100

    Worked on fixing the repsonsiveness issue on tablet devices

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,11 @@ import News from "../components/News";
 import SideNav from "../components/SideNav";
 const Home = () => {
   return (
-    <section className="flex flex-row justify_between p-4 w-11/12 sm:p-2">
-      <div className="basis-1/6">
+    <section className="flex flex-row justify_between p-4 w-11/12 md:flex-col">
+      <div className="basis-1/6 md:fixed md:bottom-0 ">
         <SideNav />
       </div>
-      <div className="basis-5/6 ">
+      <div className="basis-5/6  ">
         <News />
       </div>
     </section>
