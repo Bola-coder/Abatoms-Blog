79da6d0cde1613e05470b04424165d69b132a78e
Ahmed Bolarinwa <bolarinwaahmed22@gmail.com>
2023-08-24 18:56:36 +0100

    Trying to see recent change update

--- components/SideNav.jsx
+++ components/SideNav.jsx
@@ -6,15 +6,15 @@ const SideNav = () => {
       className="flex flex-col h-[95vh] border-green-400 border-r-2 fixed w-[7vw
     ] md:flex-row md:h-[12vh] md:w-[90vw] md:m-auto md:bottom-0 md:left-[5vw] md:border-2 md:z-50 md:bg-slate-900 "
     >
-      <div className="basis-3/5 md:basis-4/5 md:flex">
-        <div className="p-3 bg-green-400 w-10/12 rounded-full text-center mt-5 cursor-pointer md:w-12 md:h-12  md:mt-2 md:ml-4">
+      <div className="basis-3/5 md:basis-3/4 md:flex md:justify-evenly">
+        <div className="p-3 bg-green-400 w-10/12 rounded-full text-center mt-5 cursor-pointer md:w-12 md:h-12  md:mt-2">
           <h3 className="text-xl font-bold">B</h3>
         </div>
-        <div className="flex flex-col justify-center items-center mt-9 w-7/12 text-center cursor-pointer md:mt-0">
+        <div className="flex flex-col justify-center items-center mt-9 w-7/12 text-center cursor-pointer md:basis-1/3 md:mt-0">
           <FaSearch className="text-2xl text-green-400" />
           <p>Search</p>
         </div>
-        <div className="flex flex-col justify-center items-center mt-9 w-7/12 text-center cursor-pointer md:mt-0 ">
+        <div className="flex flex-col justify-center items-center mt-9 w-7/12 text-center cursor-pointer md:basis-1/3 md:mt-0 ">
           {/* <FaArrow className="text-2xl text-green-400" /> */}
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -33,7 +33,7 @@ const SideNav = () => {
           <p>Trending</p>
         </div>
       </div>
-      <div className="basis-2/5 md:basis-1/5 md:flex md:flex-row ">
+      <div className="basis-2/5 md:basis-1/4 md:flex md:flex-row ">
         {/* <div className="flex flex-col justify-center items-center mt-9 w-7/12 text-center cursor-pointer md:mt-0 ">
           <FaPlusCircle className="text-2xl text-green-400" />
           <p>Create</p>
